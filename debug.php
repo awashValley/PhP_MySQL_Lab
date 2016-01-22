86933d26129122c6bde453e64eaf98841bd8cba3
awashValley <hammerson1@gmail.com>
2016-01-22 08:45:24 +0100

    Update debug.php

--- debug.php
+++ debug.php
@@ -1,5 +1,5 @@
 * Git pull is not possible, because of unmerged files. ;
-git reset --hard
-git pull --rebase
-git rebase --skip
-git pull
+  git reset --hard
+  git pull --rebase
+  git rebase --skip
+  git pull
