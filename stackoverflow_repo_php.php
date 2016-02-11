86bf3e3b294fccad93058ce6d32460490d63fbff
awashValley <hammerson1@gmail.com>
2016-02-11 12:33:32 +0100

    Update stackoverflow_repo_php.php

--- stackoverflow_repo_php.php
+++ stackoverflow_repo_php.php
@@ -1,5 +1,5 @@
 /* When to use self over $this?
-   [Amal Murali & John Millikin, Jan 24, 2014]
+   [Amal Murali and John Millikin, Jan 24, 2014]
    -> Use $this to refer to the current object. Use self to refer to the current class. 
    -> In other words, use $this->member for non-static members, use self::$member for static members.
 */
