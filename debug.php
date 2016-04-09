4c9c051d3cec6ba8787193292a55a4e74738e749
awashValley <hammerson1@gmail.com>
2016-04-09 21:35:55 +0200

    Update debug.php

--- debug.php
+++ debug.php
@@ -1,3 +1,9 @@
 
 // Parse error: syntax error, unexpected T_IF in
    DEBUG: check if a semi-colon is forgotten...
+
+// [Sat 09Apr2016]. Error: Cannot Access Empty Property in PHP...
+   function reserve($reservationDate){
+  		$this->$date =$reservationDate;   // WRRONG - remove $ before 'date'
+  		$this->date =$reservationDate;   // hola
+  	}
